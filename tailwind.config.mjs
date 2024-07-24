15dc34e589daad470e80ea576df3c4108893a680
JustBecerra <justj.becerra@gmail.com>
2024-07-24 15:12:23 -0300

    add tailwind color and font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        black: "#000000",
+        white: "#ffffff",
+        yellow: {
+          20: "#FFC400",
+        },
+        orange: {
+          20: "#FF5D0D",
+        },
+      },
+      fontFamily: {
+        atkinson: ["Atkinson", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
